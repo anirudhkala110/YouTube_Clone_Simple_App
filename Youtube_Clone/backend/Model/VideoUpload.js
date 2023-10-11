0b2d8e957a4a2d373febc92872dbb318ae8da7d8
Anirudh Kala <122237860+anirudhkala110@users.noreply.github.com>
2023-10-12 04:11:27 +0530

    Update VideoUpload.js

--- Youtube_Clone/backend/Model/VideoUpload.js
+++ Youtube_Clone/backend/Model/VideoUpload.js
@@ -4,34 +4,48 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import { Sequelize, DataTypes } from 'sequelize';
-
+import sequelizeMongoose from 'sequelize-mongoose';
 // Initialize Express app
-const app = express();
-app.use(cors({
-    // ... cors configuration
-}));
-app.use(cookieParser());
+// const app = express();
+// app.use(cors({
+//     // ... cors configuration
+// }));
+// app.use(cookieParser());
 
 // Load environment variables
-dotenv.config();
+// dotenv.config();
 
 // Set up Sequelize and define UserModel
-const sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    logging: false,//This will pause the regular Message on executing the frontend so that backend doesn't get too much busy 
-    /* 
-    Executing (default): SELECT `id`,
-     `userId`, `title`, `description`,
-      `file`, `postedby`, `createdAt`,
-       `updatedAt` FROM `posts` 
-        AS `posts`;
-    This is the messege that is showing every render from the front end and it is too much annoying when you use the useEffect for the re-rendering
-        */
+// const sequelize = new Sequelize({
+//     dialect: 'mysql',
+//     host: process.env.DATABASE_HOST,
+//     database: process.env.DATABASE,
+//     username: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASS,
+//     logging: false,//This will pause the regular Message on executing the frontend so that backend doesn't get too much busy 
+//     /* 
+//     Executing (default): SELECT `id`,
+//      `userId`, `title`, `description`,
+//       `file`, `postedby`, `createdAt`,
+//        `updatedAt` FROM `posts` 
+//         AS `posts`;
+//     This is the messege that is showing every render from the front end and it is too much annoying when you use the useEffect for the re-rendering
+//         */
+// });
+const mongodbUri = 'mongodb+srv://anirudhkala110:Akala@12345@atlascluster.4d6nup2.mongodb.net/yt_clone?retryWrites=true&w=majority';
+mongoose.connect(mongodbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+const MongooseAdapter = sequelizeMongoose(mongoose);
+const sequelize = new Sequelize({
+  dialect: 'mssql',  // The dialect is set to a placeholder (it doesn't matter in this case)
+  define: {
+    // Sequelize configurations if needed
+  },
+});
+
+
 const VideoUploadModel = sequelize.define('VideoUpload', {
     vid: {
         type: DataTypes.INTEGER,
@@ -73,7 +87,7 @@ const VideoUploadModel = sequelize.define('VideoUpload', {
     },
 })
 
-sequelize.sync({ force: false }) // This will create the table if it doesn't exist and if exists then delete the old one and then create hance we use false in place of true so make it ture for the first run and then turn to false
+sequelize.sync({ force: true }) // This will create the table if it doesn't exist and if exists then delete the old one and then create hance we use false in place of true so make it ture for the first run and then turn to false
     .then(() => {
         console.log('\nVideo Tables synchronized\n');
     })
